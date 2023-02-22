a89ef2395930bcce3030f639f8df557f575b974a
ArkhanLupfi <arkhanlupfi6@gmail.com>
2023-02-22 20:36:44 +0700

    menambahkan file

---
+++ src/pages/LandingPages.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+import moduleName from 'elements/Button'
+export default class LandingPages extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
